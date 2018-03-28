c28fa9abefab456aa601d0b831897438c858c5ee
QwerTech <pavel_novikov1@epam.com>
2018-03-29 00:03:08 +0300

    try fix build

--- src/main/java/ru/muzetray/zsearch/zapi/TokenStore.java
+++ src/main/java/ru/muzetray/zsearch/zapi/TokenStore.java
@@ -1,24 +1,30 @@
 package ru.muzetray.zsearch.zapi;
 
-import lombok.NoArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import javax.annotation.PostConstruct;
+
 import org.springframework.stereotype.Service;
 import ru.muzetray.zsearch.services.SearchByArtistService;
 
 @Service
 public class TokenStore {
-    private String token;
+	private String token;
 
-    private SearchByArtistService service;
+	private SearchByArtistService service;
 
-    public TokenStore(SearchByArtistService service) {
-        this.service = service;
-    }
+	public TokenStore(SearchByArtistService service) {
+		this.service = service;
+	}
 
-    public String getToken() {
-        if (token != null)
-            return token;
-        token = service.getToken();
-        return token;
-    }
+	@PostConstruct
+	public String getToken() {
+		if ( token != null ) {
+			return token;
+		}
+		synchronized (this) {
+			if ( token == null ) {
+				token = service.getToken();
+			}
+		}
+		return token;
+	}
 }
