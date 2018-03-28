10732eb98ca43eed0f61f0bbdc86e546c4ebdb09
QwerTech <pavel_novikov1@epam.com>
2018-03-28 23:48:13 +0300

    test access to zapi

--- src/main/java/ru/muzetray/zsearch/zapi/FeignZapiConfiguration.java
+++ src/main/java/ru/muzetray/zsearch/zapi/FeignZapiConfiguration.java
@@ -1,16 +1,26 @@
 package ru.muzetray.zsearch.zapi;
 
 import feign.RequestInterceptor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Lazy;
 
+@Slf4j
 public class FeignZapiConfiguration {
 
-    @Bean
-    public RequestInterceptor accessTokenAuthRequestInterceptor(@Lazy TokenStore store) {
-        String debug_token = "WVh18sEasrVgy7CkM8ryE0ljqgXb7EiF2dhoYdzIJnad3vzuqMe8SsBVEWu-kc3R8gHNuUbwhRYpdKcDUNrzMCaKh8mHqrGXP8EFW94kTn0JzmHNSbcfBsQ2IvUu5nAfPYaEW1nggoNSo8spQSwQuh9-oH2-al5C7sQWi2B90NJGXjpTC4ygj-KODAlLsOPQ";
-        return template -> {
-            template.query("access_token", store.getToken());
-        };
-    }
+	@Bean
+	public RequestInterceptor accessTokenAuthRequestInterceptor(@Lazy TokenStore store) {
+		String debug_token = "WVh18sEasrVgy7CkM8ryE0ljqgXb7EiF2dhoYdzIJnad3vzuqMe8SsBVEWu-kc3R8gHNuUbwhRYpdKcDUNrzMCaKh8mHqrGXP8EFW94kTn0JzmHNSbcfBsQ2IvUu5nAfPYaEW1nggoNSo8spQSwQuh9-oH2-al5C7sQWi2B90NJGXjpTC4ygj-KODAlLsOPQ";
+		return template -> {
+			template.query( "access_token", store.getToken() );
+		};
+	}
+
+	@Bean
+	public RequestInterceptor loggingRequestInterceptor() {
+		return template ->
+				log.debug( "Requesting {}:{}", template.method(), template.url() );
+
+	}
+
 }
